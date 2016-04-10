2969e641036dbc78d2fd87014947f6f61e2172c5
vmanamino <vmanamino@gmail.com>
2016-04-10 00:45:59 +0000

    preparing api call

---
+++ js/app.js
@@ -0,0 +1,12 @@
+
+function getEstablishment(){
+    // console.log("get establishment");
+    
+}
+
+
+$(document).ready( function() {
+    // console.log("document ready!");
+    getEstablishment();
+    
+});
