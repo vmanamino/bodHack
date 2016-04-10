61d52a562ec4a8d161ac3518391acfb06df96398
vmanamino <vmanamino@gmail.com>
2016-04-10 03:44:45 +0000

    requests working

--- js/app.js
+++ js/app.js
@@ -1,12 +1,45 @@
-
-function getEstablishment(){
+function getZip(){
     // console.log("get establishment");
     
+    var request = {
+        $query: 'SELECT businessname, address, zip, violdesc, comments, result, resultdttm, violation WHERE zip=\'02113\''
+    };
+    
+    $.ajax({
+        url: "https://data.cityofboston.gov/resource/qndu-wx8w",
+        data: request,
+        dataType: 'json',
+        type: 'GET'
+    })
+    .done(function(response){
+    //   console.log(response); 
+    });
+}
+
+
+
+function getEstablishment(establishment){
+    // console.log("get establishment");
+    
+    var request = {
+        $query: 'SELECT businessname, address, zip, violdesc, comments, result, resultdttm, violation WHERE businessname=\''+establishment+'\''
+    };
+    
+    $.ajax({
+        url: "https://data.cityofboston.gov/resource/qndu-wx8w",
+        data: request,
+        dataType: 'json',
+        type: 'GET'
+    })
+    .done(function(response){
+        console.log(response); 
+    });
 }
 
 
 $(document).ready( function() {
     // console.log("document ready!");
-    getEstablishment();
+    var establishment = "Pasta & Pomodoro";
+    getEstablishment(establishment);
     
 });
