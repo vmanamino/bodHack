b68806c0dc32920bb0a7dd0637e51a962348d66c
vmanamino <vmanamino@gmail.com>
2016-04-10 05:35:06 +0000

    omitted unneccesary function, added ORDER BY

--- js/app.js
+++ js/app.js
@@ -1,28 +1,10 @@
-function getZip(){
+
+
+function getZip(zip){
     // console.log("get establishment");
     
     var request = {
-        $query: 'SELECT businessname, address, zip, violdesc, comments, result, resultdttm, violation WHERE zip=\'02113\''
-    };
-    
-    $.ajax({
-        url: "https://data.cityofboston.gov/resource/qndu-wx8w",
-        data: request,
-        dataType: 'json',
-        type: 'GET'
-    })
-    .done(function(response){
-    //   console.log(response); 
-    });
-}
-
-
-
-function getEstablishment(establishment){
-    // console.log("get establishment");
-    
-    var request = {
-        $query: 'SELECT businessname, address, zip, violdesc, comments, result, resultdttm, violation WHERE businessname=\''+establishment+'\''
+        $query: 'SELECT businessname, address, zip, violdesc, comments, result, resultdttm, violation WHERE zip=\''+zip+'\' ORDER BY resultdttm DESC'
     };
     
     $.ajax({
@@ -37,9 +19,39 @@ function getEstablishment(establishment){
 }
 
 
+
+// function getEstablishment(establishment){
+//     // console.log("get establishment");
+    
+//     var request = {
+//         $query: 'SELECT businessname, address, zip, violdesc, comments, result, resultdttm, violation WHERE businessname=\''+establishment+'\''
+//     };
+    
+//     $.ajax({
+//         url: "https://data.cityofboston.gov/resource/qndu-wx8w",
+//         data: request,
+//         dataType: 'json',
+//         type: 'GET'
+//     })
+//     .done(function(response){
+//         console.log(response); 
+//     });
+// }
+
+
 $(document).ready( function() {
     // console.log("document ready!");
-    var establishment = "Pasta & Pomodoro";
-    getEstablishment(establishment);
-    
+    // $(".est-getter").submit(function(e){
+    //     e.preventDefault();
+    //     $(".listing").html('');
+    //     var establishment = $(this).find("input[name='establishment']").val();
+    //     // establishment = "IDA'S RESTAURANT";
+    //     getEstablishment(establishment);
+    // });
+    $(".zip-getter").submit(function(e){
+        e.preventDefault();
+        $(".listing").html('');
+        var zip = $(this).find("input[name='zip']").val();
+        getZip(zip);
+    });
 });
